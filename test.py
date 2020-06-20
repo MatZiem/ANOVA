c6caf96ecfef82a308d96867b8ee83d3669aabbf
MatZiem <60630389+MatZiem@users.noreply.github.com>
2020-06-20 18:24:23 +0200

    Add files via upload

--- test.py
+++ test.py
@@ -1,33 +1,32 @@
 import numpy as np
 import pandas as pd
 from sklearn import datasets
+from sklearn import feature_selection
 import scipy.stats as stats
-from sklearn.model_selection import train_test_split
+from sklearn.feature_selection import SelectKBest, f_classif
 
-dataset = np.genfromtxt("australian.csv", delimiter=",")
-X = dataset[:, :-1]
-y = dataset[:, -1].astype(int)
+data = pd.read_csv("hepatitis.csv", header=None)
+#dataset = np.genfromtxt("hepatitis.csv", delimiter=",")
+X = data.iloc[:, :-1].values
+y = data.iloc[:, -1].values
 
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y,
-    test_size=.3,
-    random_state=42
-)
+F, p = feature_selection.f_classif(X, y)
+print(p<0.05)
+X_new = SelectKBest(f_classif, 6).fit_transform(X, y)
+#n, k = X.shape
+#x_mean = X.mean(axis = 0)
+#overall_mean = x_mean.mean()
 
-n, k = X.shape
-x_mean = X.mean(axis = 0)
-overall_mean = x_mean.mean()
+#sb = n*(x_mean-overall_mean)**2
+#ssb = sb.sum()
+#dfb = k - 1
+#msb = ssb/dfb
 
-sb = n*(x_mean-overall_mean)**2
-ssb = sb.sum()
-dfb = k - 1
-msb = ssb/dfb
+#sw = (X - x_mean)**2
+#ssw = sw.sum()
+#dfw = k*(n-1)
+#msw = ssw/dfw
+#F = msb/msw
 
-sw = (X - x_mean)**2
-ssw = sw.sum()
-dfw = k*(n-1)
-msw = ssw/dfw
-F = msb/msw
-
-alfa = 0.05
-F_crit = stats.f.ppf(1-alfa, dfb, dfw)
+#alfa = 0.05
+#F_crit = stats.f.ppf(1-alfa, dfb, dfw)
