179b31589b3c57a33ef292174f8b88affaa903e9
MatZiem <60630389+MatZiem@users.noreply.github.com>
2020-06-25 22:26:04 +0200

    Add files via upload

--- f_score.py
+++ f_score.py
@@ -8,12 +8,16 @@ class ANOVA():
         self.k = k
 
     def fit(self, X, y):
+        #sprawdza czy rozmiar X i y jest podobny
         X, y = check_X_y(X, y)
         return X, y
 
     def fit_transform(self, X, y):
+        #sprwdzenie wartości F-score i p_value dla kazdej kolumny
         F, p = f_classif(X, y)
+        #sortowanie po F-score malejaco
         idx = np.argsort(F)
         idx_t = idx[::-1]
+        #wybranie k pierwszy wartości z największym F-score
         selected_features = X[:, idx[0:self.k]]
         return selected_features
